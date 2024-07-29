5d2b3638198cf9ce4e4c6d0c8461fda47d0fd063
Simon <124474593+Gerardo-Rivera@users.noreply.github.com>
2024-07-29 08:43:44 -0600

    Cree una clase y un menú

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void Main(string[] args)
+{
+    while (true)
+    {
+        MostrarMenu();
+        int opcion;
+        if (!int.TryParse(Console.ReadLine(), out opcion))
+        {
+            Console.WriteLine("No se acepta.");
+            continue;
+        }
+
+        switch (opcion)
+        {
+            case 1:
+                
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 7:
+                Console.WriteLine("Saliendo...");
+                return;
+            default:
+                Console.WriteLine("No se acepta.");
+                break;
+        }
+
+        Console.WriteLine("\nPresiona Enter para continuar...");
+        Console.ReadLine();
+    }
+}
+
+static void MostrarMenu()
+{
+    Console.Clear();
+    Console.WriteLine("Menú de Funciones:");
+    Console.WriteLine("1. Solicitar nombre de producto");
+    Console.WriteLine("2. Ver productos");
+    Console.WriteLine("3. Salir");
+    Console.Write("Selecciona una opción: ");
+}
